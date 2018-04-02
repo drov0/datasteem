6e96cb1776725bd260abd81704e6c0f66296ce7d
Martin Lees <drov.fr@protonmail.com>
2018-04-02 11:36:49 +0200

    scaling solution

--- steemdata.js
+++ steemdata.js
@@ -6,13 +6,13 @@ const fn = promisify(connection.query).bind(connection);
 
 var es = require('event-stream') // npm install event-stream
 var util = require('util')
-const steem = new Client('https://api.steemit.com')
+const steem = new Client('https://gtg.steem.house:8090')
 
 
 
 function setupSteemjs() {
     const steem = require('steem');
-    steem.api.setOptions({url: 'https://api.steemit.com'});
+    steem.api.setOptions({url: 'https://gtg.steem.house:8090'});
     return steem;
 }
 
@@ -23,7 +23,7 @@ async function parseBlock(blocknb)
 {
     const block = await steem.database.getBlock(blocknb)
     const tx = block['transactions'];
-
+    console.log(blocknb);
     const time = (new Date(Date.parse(block['timestamp']))).getTime()/1000;
 
     for (let i = 0; i < tx.length; i++)
@@ -52,6 +52,8 @@ async function parseBlock(blocknb)
                    await fn("INSERT INTO `post` (`id`,`block_id`, `author`, `title`,`date`, `text`, `permlink`, `image`, `tag1`, `tag2`, `tag3`, `tag4`, `tag5`, `json_metadata`, `reward`, `comments`, `upvotes`, `last_updated`) VALUES(NULL,?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,-1,-1,-1,-1)",
                            [blocknb, post['author'] , post['title'], time, "", post['permlink'],img , post['parent_permlink'], (tags[1] ? tags[1] : ''), (tags[2] ? tags[2] : ''), (tags[3] ? tags[3] : ''), (tags[4] ? tags[4] : ''), post['json_metadata']])
 
+                   await fn("insert into exist(id, author, permlink) values(NULL, ?, ?)", [result[i]['author'],result[i]['permlink']])
+
                    // update/add user
                    const data = await get_user_data(post['author']);
                    await fn("INSERT INTO user(`id`, `username`, `reputation`, `steem_posts`, `steem_join`, `followers`, `following`, `sp`, `delegated_sp`, last_updated) VALUES(NULL, ?,?,?,?,?,?,?,?,?)" +
@@ -60,22 +62,24 @@ async function parseBlock(blocknb)
                            data['reputation'], data['post_count'], data['followers'], data['following'], data['sp'], data['delegated'],Math.floor(new Date().getTime() / 1000)])
 
                }
-           } /*
+           }
            // TODO : Find a way to optimize.
 
            else if (tx[i]['operations'][y][0] === "vote")
            {
+               console.log("vote");
                const vote = tx[i]['operations'][y][1];
-               const exists = await fn("select id from post where author = ? AND permlink = ?", [vote['author'], vote['permlink']]);
+               const exists = await fn("select 1 from exist where author = ? AND permlink = ?", [vote['author'], vote['permlink']]);
 
                if (exists.length !== 0) {
+                   console.log("on existing, updating post");
 
                    const data = await get_steem_data(vote['author'], vote['permlink']);
 
-                   await fn("update post set text = ?, reward = ?, comments = ?, upvotes = ?, last_updated = ? where author = ? AND permlink = ?",
-                       [data['text'], data['reward'], data['comments'], data['upvotes'], time, vote['author'], vote['permlink']]);
+                   await fn("update post set reward = ?, comments = ?, upvotes = ?, last_updated = ? where author = ? AND permlink = ?",
+                       [data['reward'], data['comments'], data['upvotes'], time, vote['author'], vote['permlink']]);
             }
-           }*/
+           }
         }
     }
 
@@ -170,9 +174,9 @@ async function update_user()
         for (let i = 0; i < users.length; i++) {
             if (i%50 === 0)
                 console.log("updating user data "+i+"/"+users.length)
-            const data = await get_user_data(users[i]['author']);
-            await fn("UPDATE reputation = ?, steem_posts = ?, followers = ?, following = ?, sp = ?, delegated_sp = ?, last_updated = ? WHERE username = ?",
-                [data['reputation'], data['post_count'], data['followers'], data['following'], data['sp'], data['delegated'],Math.floor(new Date().getTime() / 1000),users[i]['author'] ])
+            const data = await get_user_data(users[i]['username']);
+            await fn("UPDATE user SET reputation = ?, steem_posts = ?, followers = ?, following = ?, sp = ?, delegated_sp = ?, last_updated = ? WHERE username = ?",
+                [data['reputation'], data['post_count'], data['followers'], data['following'], data['sp'], data['delegated'],Math.floor(new Date().getTime() / 1000), users[i]['username'] ])
         }
 
         console.log("finished updating " + users.length.toString() + " users in " + (Math.floor(new Date().getTime() / 1000) - now).toString() + "seconds");
@@ -220,7 +224,7 @@ async function main() {
 
     let lastblock = await fn("SELECT DISTINCT block_id FROM `post` order by block_id desc LIMIT 1");
 
-    if (lastblock.length === 1) {
+    if (lastblock.length !== 1) {
         lastblock = lastblock[0]['block_id'];
         var stream = steem.blockchain.getBlockNumberStream({from: lastblock});
     } else
@@ -233,14 +237,30 @@ async function main() {
 
     while (true)
     {
-        const posts_count = await update_post();
+        //const posts_count = await update_post();
         const user_count = await update_user();
-        if (posts_count === 0 && user_count === 0)
+        if (user_count === 0)
             await wait(15); // we are up to date, waiting one block
     }
 }
 
 main();
+
+
+async function test()
+{
+    var result = await fn("select author,permlink from post");
+
+    for (let i = 0; i < result.length; i++)
+    {
+        if (i % 500 === 0)
+            console.log(i)
+        await fn("insert into exist(id, author, permlink) values(NULL, ?, ?)", [result[i]['author'],result[i]['permlink']])
+    }
+
+}
+
+//test()
 
 
 
